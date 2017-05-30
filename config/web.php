3d1f75e2b694151e8cc1b604c1ab04436c79c143
Ricardo Pérez <ricpelo@gmail.com>
2017-05-30 16:52:14 +0200

    Cambia el log

--- config/web.php
+++ config/web.php
@@ -44,7 +44,7 @@ $config = [
             'traceLevel' => YII_DEBUG ? 3 : 0,
             'targets' => [
                 [
-                    'class' => 'yii\log\FileTarget',
+                    'class' => 'yii\log\SyslogTarget',
                     'levels' => ['error', 'warning'],
                 ],
             ],
